de9766ef66dadc8905aa08fc70560d262c834867
thanhson539 <sonntph30566@fpt.edu.vn>
2023-10-10 09:53:18 +0700

    note

--- src/navigation/stackNavigation.js
+++ src/navigation/stackNavigation.js
@@ -1,17 +1,11 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreens from "../screens/HomeScreens";
-<<<<<<< HEAD
 import bottomNav from "./botomNavigation"
-<<<<<<< HEAD
 import MyInformationComponent from "../component/usercomponent/MyInformationComponent";
-=======
 import Header from "../component/homecomponent/Header";
 import { SafeAreaView } from "react-native-safe-area-context";
->>>>>>> dd582f5ce90adad23a354c7235babff2538a25b3
-=======
 import bottomNav from "./botomNavigation";
-
 import FlashSceen from "../screens/FlashSceen";
 import SignIn from "../auth/SignIn";
 import SighUp from "../auth/SighUp";
@@ -21,27 +15,13 @@ import GetHelp from "../auth/GetHelp";
 import Header from "../component/homecomponent/Header";
 import { SafeAreaView } from "react-native-safe-area-context";
 
->>>>>>> 838241bde279ad6e973a0884f46151466f4cc117
+
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="HomeScreens" component={bottomNav} />
-        <Stack.Screen name="MyInformationScreens" component={MyInformationComponent}/>
-=======
-    <Stack.Navigator screenOptions={{
-      headerStyle: {
-        width: '100%',
-        height: 120
-      },
-      header: () => <Header />
-    }}>
-      <Stack.Screen name="HomeScreens" component={bottomNav} />
->>>>>>> dd582f5ce90adad23a354c7235babff2538a25b3
-=======
+
+   
     <Stack.Navigator
       screenOptions={{
         headerStyle: {
@@ -57,7 +37,7 @@ function MyStack() {
       <Stack.Screen name="SignUP" component={SighUp} />
       <Stack.Screen name="FogotPass" component={FogotPass} />
       <Stack.Screen name="GetHelp" component={GetHelp} />
->>>>>>> 838241bde279ad6e973a0884f46151466f4cc117
+
     </Stack.Navigator>
   );
 }
